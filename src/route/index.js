f9c0c1d3378dff1b728ac24c31016e09d4e9ecfe
vbuzanov <vbuzanov@ukr.net>
2023-04-20 20:30:06 +0300

    bootstr8

--- src/route/index.js
+++ src/route/index.js
@@ -1123,7 +1123,7 @@ router.get('/bootstrap', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('bootstrapOld3', {
+  res.render('bootstrap', {
     layout: 'bootstrap',
   })
   //                  ↑↑ сюди вводимо JSON дані
